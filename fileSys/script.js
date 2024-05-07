1f930d4a8021bc8cae6473d27ab5fd84ea079f1f
jushymaso222 <92840402+jushymaso222@users.noreply.github.com>
2024-05-07 08:17:31 -0400

    fix file sys

--- fileSys/script.js
+++ fileSys/script.js
@@ -40,13 +40,16 @@ function butts(e)
 }
 
 function deets(e) {
-    var svg = e.target.querySelector(`path`)
-    e.target.classList.toggle(`activeTab`)
+    if (e.target.nodeName != "LI") {
+        var svg = e.target.querySelector(`path`)
+        e.target.classList.toggle(`activeTab`)
+        console.log(e.target.nodeName)
 
-    if (e.target.classList.contains(`activeTab`)) {
-        svg.setAttribute('d', "M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z");
-    } else {
-        svg.setAttribute('d', "M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z");
+        if (e.target.classList.contains(`activeTab`)) {
+            svg.setAttribute('d', "M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z");
+        } else {
+            svg.setAttribute('d', "M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z");
+        }
     }
 }
 
